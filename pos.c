986607f4cc33df35018f0343779066309ab0cad8
Jeyanth-k <30952763+Jeyanth-k@users.noreply.github.com>
2017-08-17 17:02:26 +0530

    c program
    positive/negative

---
+++ pos.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a:
+printf("\n enter the number");
+scanf("%d");
+if(a>0)
+{
+printf("\n enter the no positive");
+break;
+}
+else(a<0)
+{
+printf("\n enter the no negative");
+break;
+}
+getch();
+}
